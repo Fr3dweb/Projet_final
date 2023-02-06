01d39232dcbffa2dc42ff47a7b276e2a42a26e00
Fr3dweb <fr3d.web@gmail.com>
2023-02-06 16:48:49 +0100

    fonction mot de passe oublié ok ( reste détails des msgs invalid etc qui sont en anglais)

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -160,7 +160,7 @@ class ResetPasswordController extends AbstractController
             ->from(new Address('admin@longoformation.com', 'LongoFormation'))
             ->to($user->getEmail())
             ->subject('Your password reset request')
-            ->htmlTemplate('reset_password/email.html.twig')
+            ->htmlTemplate('reset_password/Email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
             ])
